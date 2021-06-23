70e4e76521930d570294e626819601ca03f4e157
Elizabeth Phung <phung.ej@gmail.com>
2021-06-23 12:22:37 -0700

    Refactor schema.sql to remove product features junction table (no longer needed)

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -2,14 +2,14 @@ CREATE DATABASE products;
 
 USE products;
 
-DROP TABLE IF EXISTS `product`, `related_product`, `feature`, `product_features`, `style`, `photo`, `sku`;
+DROP TABLE IF EXISTS `product`, `related_products`, `features`, `styles`, `photos`, `skus`;
 
 CREATE TABLE `product` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(20) NOT NULL,
   `slogan` VARCHAR(120) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
-  `default_price` DECIMAL(6,2) NOT NULL,
+  `default_price` DECIMAL(11,2) NOT NULL,
   `category` ENUM(
     'Backpack', 'Basketball Shoes', 'Boots', 'Cap', 'Coat', 'Dress Shoes', 'Dress', 'Hat', 'Heels', 'Hoodie', 'Jackets', 'Kicks', 'Pants', 'Romper', 'Shirt', 'Shoes', 'Shorts', 'Skirt', 'Slacks', 'Socks', 'Suit', 'Sunglasses', 'Sweater', 'Sweatpants', 'Tank Top', 'Trousers'
     ) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE `product` (
   PRIMARY KEY (`id`),
 );
 
-CREATE TABLE `related_product` (
+CREATE TABLE `related_products` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
   `related_product_id` SMALLINT NOT NULL,
   `product_id` SMALLINT NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE `related_product` (
   INDEX (`product_id`),
 );
 
-CREATE TABLE `feature` (
+CREATE TABLE `features` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
   `feature` ENUM(
     '5 Year Warranty', 'Buttons', 'Cut', 'Fabric', 'Fair Trade Certified', 'Frame', 'Frames', 'Green Leaf Certified', 'Lens', 'Lenses', 'Lifetime Guarantee', 'Material', 'Mid-Sole', 'Non-GMO', 'Satisfaction Guaranteed', 'Sole', 'Stitching', 'Sustainably Sourced', 'UV Protection'
@@ -36,31 +36,25 @@ CREATE TABLE `feature` (
   `value` ENUM(
     '100% Cotton', '100% UV Protective', '80% Cotton', '95% Cotton', '99% Cotton 1% Elastic', 'AllLight Composition Resin', 'Armor Weave', 'Black Resin', 'Blue Resin', 'Brass', 'Canvas', 'Cashmere', 'Control Support Bridge', 'ControlSupport Arch Bridge', 'Cool Fit', 'Cross Stitch', 'Double Stitch', 'DuraResin', 'FullControl Skin', 'FullControlSkin', 'FullSupport Hybrid Compound', 'Ivory', 'LightCompose', 'Loose', 'Rubber Mesh', 'Rubber', 'Silk', 'Skinny', 'Straight', 'Striaght', 'Ultrasheen Basic', 'Ultrasheen Gold', 'Ultrasheen Silver', 'Ultrasheen', 'Velvet', 'White Resin', 'Wool'
     ) DEFAULT NULL,
+    `product_id` SMALLINT NOT NULL,
 
   PRIMARY KEY (`id`),
+  INDEX (`product_id`),
 );
 
-CREATE TABLE `product_features` (
-  `feature_id` SMALLINT NOT NULL,
-  `product_id` SMALLINT NOT NULL,
-
-  PRIMARY KEY (`feature_id`, `product_id`),
-  INDEX (`product_id`, `feature_id`),
-);
-
-CREATE TABLE `style` (
+CREATE TABLE `styles` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
   `default_style` BOOL DEFAULT 0,
-  `original_price` DECIMAL(6,2) NOT NULL,
-  `sale_price` DECIMAL(6,2) DEFAULT NULL,
+  `original_price` DECIMAL(11,2) NOT NULL,
+  `sale_price` DECIMAL(11,2) DEFAULT NULL,
   `product_id` SMALLINT NOT NULL,
 
   PRIMARY KEY (`id`),
   INDEX (`product_id`),
 );
 
-CREATE TABLE `photo` (
+CREATE TABLE `photos` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
   `url` VARCHAR(250) NOT NULL,
   `thumbnail_url` VARCHAR(250) NOT NULL,
@@ -70,7 +64,7 @@ CREATE TABLE `photo` (
   INDEX (`style_id`),
 );
 
-CREATE TABLE `sku` (
+CREATE TABLE `skus` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
   `quantity` SMALLINT NOT NULL,
   `size` ENUM(
@@ -82,9 +76,8 @@ CREATE TABLE `sku` (
   INDEX (`style_id`, `size`),
 );
 
-ALTER TABLE `related_product` ADD FOREIGN KEY (current_product_id) REFERENCES `product` (`id`);
-ALTER TABLE `product_features` ADD FOREIGN KEY (feature_id) REFERENCES `feature` (`id`);
-ALTER TABLE `product_features` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
-ALTER TABLE `style` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
-ALTER TABLE `photo` ADD FOREIGN KEY (style_id) REFERENCES `style` (`id`);
-ALTER TABLE `sku` ADD FOREIGN KEY (style_id) REFERENCES `style` (`id`);
+ALTER TABLE `related_products` ADD FOREIGN KEY (current_product_id) REFERENCES `product` (`id`);
+ALTER TABLE `features` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
+ALTER TABLE `styles` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
+ALTER TABLE `photos` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
+ALTER TABLE `skus` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
