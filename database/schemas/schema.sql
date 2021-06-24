43785f9089a274673f32781daab706b6e913c315
Elizabeth Phung <phung.ej@gmail.com>
2021-06-23 22:54:52 -0700

    Refactored to address syntax error with related_products name

--- database/schemas/schema.sql
+++ database/schemas/schema.sql
@@ -1,82 +1,84 @@
+DROP DATABASE IF EXISTS products;
+
 CREATE DATABASE products;
 
 USE products;
 
-DROP TABLE IF EXISTS `product`, `related_products`, `features`, `styles`, `photos`, `skus`;
+DROP TABLE IF EXISTS product, related, features, styles, photos, skus;
 
-CREATE TABLE `product` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(20) NOT NULL,
-  `slogan` VARCHAR(120) NOT NULL,
-  `description` VARCHAR(500) NOT NULL,
-  `category` VARCHAR(30) NOT NULL,
-  `default_price` DECIMAL(11,2) NOT NULL,
-  `created_at` DATE DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` DATE DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+CREATE TABLE product (
+  id SMALLINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(20) DEFAULT NULL,
+  slogan VARCHAR(120) DEFAULT NULL,
+  description VARCHAR(500) DEFAULT NULL,
+  category VARCHAR(30) DEFAULT NULL,
+  default_price DECIMAL(11,2) DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE `related_products` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `current_product_id` SMALLINT NOT NULL,
-  `related_product_id` SMALLINT NOT NULL,
+CREATE TABLE related (
+  id SMALLINT NOT NULL AUTO_INCREMENT,
+  current_product_id SMALLINT DEFAULT NULL,
+  related_product_id SMALLINT DEFAULT NULL,
 
-  PRIMARY KEY (`id`),
-  -- INDEX (`product_id`),
+  PRIMARY KEY (id)
+  -- INDEX (product_id)
 );
 
-CREATE TABLE `features` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `product_id` SMALLINT NOT NULL,
-  `feature` VARCHAR(30) NOT NULL,
-  `value` VARCHAR(30) DEFAULT NULL,
+CREATE TABLE features (
+  id SMALLINT NOT NULL AUTO_INCREMENT,
+  product_id SMALLINT DEFAULT NULL,
+  feature VARCHAR(30) DEFAULT NULL,
+  value VARCHAR(30) DEFAULT NULL,
 
-  PRIMARY KEY (`id`),
-  -- INDEX (`product_id`),
+  PRIMARY KEY (id)
+  -- INDEX (product_id)
 );
 
-CREATE TABLE `styles` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `product_id` SMALLINT NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  `sale_price` DECIMAL(11,2) DEFAULT NULL,
-  `original_price` DECIMAL(11,2) NOT NULL,
-  `default_style` BOOL DEFAULT 0,
+CREATE TABLE styles (
+  id SMALLINT NOT NULL AUTO_INCREMENT,
+  product_id SMALLINT DEFAULT NULL,
+  name VARCHAR(100) DEFAULT NULL,
+  sale_price DECIMAL(11,2) DEFAULT NULL,
+  original_price DECIMAL(11,2) DEFAULT NULL,
+  default_style BOOL DEFAULT 0,
 
-  PRIMARY KEY (`id`),
-  -- INDEX (`product_id`),
+  PRIMARY KEY (id)
+  -- INDEX (product_id)
 );
 
-CREATE TABLE `photos` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `style_id` SMALLINT NOT NULL,
-  `url` VARCHAR(500) NOT NULL,
-  `thumbnail_url` VARCHAR(500) NOT NULL,
+CREATE TABLE photos (
+  id SMALLINT NOT NULL AUTO_INCREMENT,
+  style_id SMALLINT DEFAULT NULL,
+  url VARCHAR(500) DEFAULT NULL,
+  thumbnail_url VARCHAR(500) DEFAULT NULL,
 
-  PRIMARY KEY (`id`),
-  -- INDEX (`style_id`),
+  PRIMARY KEY (id)
+  -- INDEX (style_id)
 );
 
-CREATE TABLE `skus` (
-  `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `style_id` SMALLINT NOT NULL,
-  `size` VARCHAR(10) NOT NULL,
-  `quantity` SMALLINT NOT NULL,
+CREATE TABLE skus (
+  id SMALLINT NOT NULL AUTO_INCREMENT,
+  style_id SMALLINT DEFAULT NULL,
+  size VARCHAR(10) DEFAULT NULL,
+  quantity SMALLINT DEFAULT NULL,
 
-  PRIMARY KEY(`id`),
-  -- INDEX (`style_id`, `size`),
+  PRIMARY KEY(id)
+  -- INDEX (style_id, size)
 );
 
--- ALTER TABLE `related_products` ADD FOREIGN KEY (current_product_id) REFERENCES `product` (`id`);
--- ALTER TABLE `features` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
--- ALTER TABLE `styles` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
--- ALTER TABLE `photos` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
--- ALTER TABLE `skus` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
+-- ALTER TABLE related_products ADD FOREIGN KEY (current_product_id) REFERENCES product (id);
+-- ALTER TABLE features ADD FOREIGN KEY (product_id) REFERENCES product (id);
+-- ALTER TABLE styles ADD FOREIGN KEY (product_id) REFERENCES product (id);
+-- ALTER TABLE photos ADD FOREIGN KEY (style_id) REFERENCES styles (id);
+-- ALTER TABLE skus ADD FOREIGN KEY (style_id) REFERENCES styles (id);
 
 -- LOAD DATA
 --   INFILE 'product.csv'
---   INTO TABLE `product`
+--   INTO TABLE product
 --   FIELDS TERMINATED BY ','
 --   ENCLOSED BY '"'
 --   LINES TERMINATED BY '\r\n'
@@ -84,7 +86,7 @@ CREATE TABLE `skus` (
 
 -- LOAD DATA
 --   INFILE 'related.csv'
---   INTO TABLE `related_products`
+--   INTO TABLE related_products
 --   FIELDS TERMINATED BY ','
 --   ENCLOSED BY '"'
 --   LINES TERMINATED BY '\r\n'
@@ -92,7 +94,7 @@ CREATE TABLE `skus` (
 
 -- LOAD DATA
 --   INFILE 'features.csv'
---   INTO TABLE `features`
+--   INTO TABLE features
 --   FIELDS TERMINATED BY ','
 --   ENCLOSED BY '"'
 --   LINES TERMINATED BY '\r\n'
@@ -100,7 +102,7 @@ CREATE TABLE `skus` (
 
 -- LOAD DATA
 --   INFILE 'styles.csv'
---   INTO TABLE `styles`
+--   INTO TABLE styles
 --   FIELDS TERMINATED BY ','
 --   ENCLOSED BY '"'
 --   LINES TERMINATED BY '\r\n'
@@ -108,7 +110,7 @@ CREATE TABLE `skus` (
 
 -- LOAD DATA
 --   INFILE 'photos.csv'
---   INTO TABLE `photos`
+--   INTO TABLE photos
 --   FIELDS TERMINATED BY ','
 --   ENCLOSED BY '"'
 --   LINES TERMINATED BY '\r\n'
@@ -116,7 +118,7 @@ CREATE TABLE `skus` (
 
 -- LOAD DATA
 --   INFILE 'skus.csv'
---   INTO TABLE `skus`
+--   INTO TABLE skus
 --   FIELDS TERMINATED BY ','
 --   ENCLOSED BY '"'
 --   LINES TERMINATED BY '\r\n'
