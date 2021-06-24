323cfb271aba737dd8ef0d78e755c07de5080bdd
Elizabeth Phung <phung.ej@gmail.com>
2021-06-23 21:43:04 -0700

    Preparing schema for loading process

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -9,8 +9,8 @@ CREATE TABLE `product` (
   `name` VARCHAR(20) NOT NULL,
   `slogan` VARCHAR(120) NOT NULL,
   `description` VARCHAR(500) NOT NULL,
-  `default_price` DECIMAL(11,2) NOT NULL,
   `category` VARCHAR(30) NOT NULL,
+  `default_price` DECIMAL(11,2) NOT NULL,
   `created_at` DATE DEFAULT CURRENT_DATE,
   `updated_at` DATE DEFAULT CURRENT_DATE,
 
@@ -19,57 +19,57 @@ CREATE TABLE `product` (
 
 CREATE TABLE `related_products` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `related_product_id` SMALLINT NOT NULL,
   `current_product_id` SMALLINT NOT NULL,
+  `related_product_id` SMALLINT NOT NULL,
 
   PRIMARY KEY (`id`),
-  INDEX (`product_id`),
+  -- INDEX (`product_id`),
 );
 
 CREATE TABLE `features` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
+  `product_id` SMALLINT NOT NULL,
   `feature` VARCHAR(30) NOT NULL,
   `value` VARCHAR(30) DEFAULT NULL,
-  `product_id` SMALLINT NOT NULL,
 
   PRIMARY KEY (`id`),
-  INDEX (`product_id`),
+  -- INDEX (`product_id`),
 );
 
 CREATE TABLE `styles` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `default_style` BOOL DEFAULT 0,
-  `original_price` DECIMAL(11,2) NOT NULL,
-  `sale_price` DECIMAL(11,2) DEFAULT NULL,
   `product_id` SMALLINT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `sale_price` DECIMAL(11,2) DEFAULT NULL,
+  `original_price` DECIMAL(11,2) NOT NULL,
+  `default_style` BOOL DEFAULT 0,
 
   PRIMARY KEY (`id`),
-  INDEX (`product_id`),
+  -- INDEX (`product_id`),
 );
 
 CREATE TABLE `photos` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
+  `style_id` SMALLINT NOT NULL,
   `url` VARCHAR(500) NOT NULL,
   `thumbnail_url` VARCHAR(500) NOT NULL,
-  `style_id` SMALLINT NOT NULL,
 
   PRIMARY KEY (`id`),
-  INDEX (`style_id`),
+  -- INDEX (`style_id`),
 );
 
 CREATE TABLE `skus` (
   `id` SMALLINT NOT NULL AUTO_INCREMENT,
-  `quantity` SMALLINT NOT NULL,
-  `size` VARCHAR(10) NOT NULL,
   `style_id` SMALLINT NOT NULL,
+  `size` VARCHAR(10) NOT NULL,
+  `quantity` SMALLINT NOT NULL,
 
   PRIMARY KEY(`id`),
-  INDEX (`style_id`, `size`),
+  -- INDEX (`style_id`, `size`),
 );
 
-ALTER TABLE `related_products` ADD FOREIGN KEY (current_product_id) REFERENCES `product` (`id`);
-ALTER TABLE `features` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
-ALTER TABLE `styles` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
-ALTER TABLE `photos` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
-ALTER TABLE `skus` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
+-- ALTER TABLE `related_products` ADD FOREIGN KEY (current_product_id) REFERENCES `product` (`id`);
+-- ALTER TABLE `features` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
+-- ALTER TABLE `styles` ADD FOREIGN KEY (product_id) REFERENCES `product` (`id`);
+-- ALTER TABLE `photos` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
+-- ALTER TABLE `skus` ADD FOREIGN KEY (style_id) REFERENCES `styles` (`id`);
