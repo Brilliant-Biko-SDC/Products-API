2ed9587f9de69f7d53ea35159a1142880e601b13
Elizabeth Phung <phung.ej@gmail.com>
2021-06-26 01:43:31 -0700

    Remove console.log

--- server/models/helpers/parseDataHelper.js
+++ server/models/helpers/parseDataHelper.js
@@ -1,5 +1,4 @@
 const parseData = (queryResults) => {
-  console.log(queryResults);
   const result = queryResults[0];
   const key = Object.keys(result)[0];
   const data = queryResults.length > 1 ? queryResults.map(product => JSON.parse(product[key])) : JSON.parse(result[key]);
