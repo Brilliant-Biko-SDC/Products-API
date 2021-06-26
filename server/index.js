36dff2301ddd7bdf8f7293a580f8879bd832f84d
Elizabeth Phung <phung.ej@gmail.com>
2021-06-26 11:51:03 -0700

    Clean up code

--- server/index.js
+++ server/index.js
@@ -4,23 +4,9 @@ const port = 3001;
 
 const routes = require('./routes.js');
 
-// * App-Level Middleware * //
-
-// Built-In Middleware
-
 app.use(express.json());
 
-// Custom Middleware
-
-// app.use((req, res, next) => {
-//   next();
-// })
-
-// * Routes * //
-
 app.use('/api/products', routes);
-
-// * Start * //
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
