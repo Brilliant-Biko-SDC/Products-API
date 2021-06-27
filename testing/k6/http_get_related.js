6bf1b39778a26aed5dd5733b10f3fe2edc464f38
Elizabeth Phung <phung.ej@gmail.com>
2021-06-27 09:22:44 -0700

    Changed K6 test thresholds, and working on configuring artillery

--- testing/k6/http_get_related.js
+++ testing/k6/http_get_related.js
@@ -2,12 +2,7 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 import { Counter } from 'k6/metrics';
 
-// A simple counter for http requests
-
 export const requests = new Counter('http_reqs');
-
-// you can specify stages of your test (ramp up/down patterns) through the options object
-// target is the number of VUs you are aiming for
 
 export const options = {
   stages: [
@@ -17,22 +12,21 @@ export const options = {
     { target: 1000, duration: '60s' },
   ],
   thresholds: {
-    requests: ['count < 100'],
+    http_req_failed: ['rate < 0.01'],
+    http_req_duration: ['avg < 50', 'p(95) < 20']
   },
 };
 
 export default function () {
-  // our HTTP request, note that we are saving the response to res, which can be accessed later
   const max = 1000011;
 
   const product_id = Math.floor(Math.random() * max) || 1;
 
-  const res = http.get(`http://localhost:3001/api/products/${product_id}`);
+  const res = http.get(`http://localhost:3001/api/products/${product_id}/related`);
 
   sleep(1);
 
   const checkRes = check(res, {
-    'status is 200': (r) => r.status === 200,
-    // 'response body': (r) => r.body.indexOf('Feel free to browse') !== -1,
+    'status is 200': (r) => r.status === 200
   });
 }
