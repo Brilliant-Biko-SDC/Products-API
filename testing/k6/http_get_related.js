003fb387a915f21dcdec118f10f8e76cf352dfad
Elizabeth Phung <phung.ej@gmail.com>
2021-06-27 16:46:15 -0700

    Adjusting stress test duration and VU to observe shorter spike period

--- testing/k6/http_get_related.js
+++ testing/k6/http_get_related.js
@@ -6,14 +6,13 @@ export const requests = new Counter('http_reqs');
 
 export const options = {
   stages: [
-    { target: 1, duration: '60s' },
-    { target: 10, duration: '60s' },
-    { target: 100, duration: '60s' },
-    { target: 1000, duration: '60s' },
+    { duration: '20s', target: 300 },
+    { duration: '1m', target: 1000 },
+    { duration: '30s', target: 0 },
   ],
   thresholds: {
     http_req_failed: ['rate < 0.01'],
-    http_req_duration: ['avg < 50', 'p(95) < 20']
+    http_req_duration: ['avg < 50', 'p(90) < 20']
   },
 };
 
