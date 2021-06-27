404c1ac56ccd6823f49ec1e583fb50af9aca1158
Elizabeth Phung <phung.ej@gmail.com>
2021-06-27 10:00:43 -0700

    Added stress test file for product request using K6, configured yaml file for artillery

---
+++ testing/k6/http_get_product_spike.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { Counter } from 'k6/metrics';
+
+export const requests = new Counter('http_reqs');
+
+export const options = {
+  stages: [
+    { duration: '10s', target: 100 }, // below normal load
+    { duration: '1m', target: 100 },
+    { duration: '10s', target: 1400 }, // spike to 1400 users
+    { duration: '3m', target: 1400 }, // stay at 1400 for 3 minutes
+    { duration: '10s', target: 100 }, // scale down. Recovery stage.
+    { duration: '3m', target: 100 },
+    { duration: '10s', target: 0 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate < 0.01'],
+    http_req_duration: ['avg < 50', 'p(95) < 20']
+  },
+};
+
+export default function () {
+  const max = 1000011;
+
+  const product_id = Math.floor(Math.random() * max) || 1;
+
+  const res = http.get(`http://localhost:3001/api/products/${product_id}`);
+
+  sleep(1);
+
+  const checkRes = check(res, {
+    'status is 200': (r) => r.status === 200
+  });
+}
