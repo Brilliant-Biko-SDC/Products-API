f4e2214277f4192132e32db5bd6951c36980b31b
Elizabeth Phung <phung.ej@gmail.com>
2021-06-30 13:09:23 -0700

    Setting up loader.io to load test server in production

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3001;
 
 const routes = require('./routes.js');
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'testing', 'loader.io')));
 
 app.use('/api/products', routes);
 
